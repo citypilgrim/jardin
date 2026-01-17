f17d31baae56ea02710158e9047dccfef6aadfd9
Lee Tianli <lee.tianli94@gmail.com>
2026-01-17 18:25:08 +0800

    add ox-hugo settings

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,6 +72,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.OxHugoFlavouredMarkdown(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
